cf8688085477b9e666041f08b8412b591d4d9fa2
mateusz swierczek <omatix2003@interia.pl>
2023-08-26 18:27:43 +0200

    server run

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.routing import APIRouter
 from Posts.PostRouter import router as post_router
 import threading
 import DB.DataBaseConnect as db 
+from uvicorn import Server, Config
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ app.include_router(post_router, prefix="/api/post")
 task = threading.Thread(target=db.InitDB())
 task.start()
 
+uvicorn_config = Config(app, host="0.0.0.0", port=8000)  # Modify host and port as needed
+server = Server(uvicorn_config)
+application = server.app
