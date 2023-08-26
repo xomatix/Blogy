a720592070d6514d3f16733994da0aec82174f8e
mateusz swierczek <omatix2003@interia.pl>
2023-08-26 09:30:14 +0200

    poprawa maina

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.routing import APIRouter
 from Posts.PostRouter import router as post_router
 import threading
 import DB.DataBaseConnect as db 
-import uvicorn
 
 app = FastAPI()
 
@@ -11,7 +10,4 @@ app.include_router(post_router, prefix="/api/post")
 
 task = threading.Thread(target=db.InitDB())
 task.start()
-
-if __name__ == "__main__":
-    uvicorn.run(app=app)
 
